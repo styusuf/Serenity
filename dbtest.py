00ef85e7557d0da57946e68cea504f825b915580
Aditya Sahai <aditya.sahai@gatech.edu>
2017-10-31 15:31:30 -0400

    improved update ingredients statement

--- dbtest.py
+++ dbtest.py
@@ -1,6 +1,7 @@
 import psycopg2 as psql
 import json
 from RecipeClass import Recipe
+from IngredientClass import Ingredient
 
 # psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
 # psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
@@ -8,23 +9,17 @@ from RecipeClass import Recipe
 conn = None
 
 try:
-	conn = psql.connect("dbname='fooddatabase' user='postgres' host='localhost' password='a' port='5433'")
+	conn = psql.connect("dbname='fooddatabaseverify' user='postgres' host='localhost' password='a' port='5433'")
 except:
 	print "unable to connect"
 	exit(1)
 
-# Open file
-with open('json_objects/20171027132145877592.json') as data_file:
-	data = json.load(data_file)
 
-
-# SQL file
-filename = 'master.sql'
-a = Recipe()
-a.populate_from_json(data)
-a.populate_insert_statement()
-# print a.insertStatement
-if a.update_with_recipe(conn, filename) == 0:
-    print "success"
-else:
-    print "fail"
+i = Ingredient(1234)
+i.id = 1007
+i.populate_statement(conn)
+c = conn.cursor()
+c.execute(i.statement, i.params)
+conn.commit()
+c.close()
+conn.close()
