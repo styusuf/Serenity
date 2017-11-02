40220a6cf1b7f78e82dcfb47e9275c06e025255f
Ryan La Fleur <ryanlafleur@lawn-143-215-55-209.lawn.gatech.edu>
2017-11-01 22:14:23 -0400

    Updated PredictQuery.py docstrings

--- PredictQuery.py
+++ PredictQuery.py
@@ -17,10 +17,15 @@ class PredictQuery:
     def train(self, X, y, linreg=False, knn=False, rf=False, k=3, knn_weights="uniform", num_trees=10, max_depth=None):
         """
         Trains indicated models. By default, trains none.
-        :param data: Training data
+        :param X: Training data features
+        :param y: Training data targets
         :param linreg: Flag indicating whether or not linear regression model should be trained
         :param knn: Flag indicating whether or not k-nearest neighbors model should be trained
         :param rf: Flag indicating whether or not random forest model should be trained
+        :param k: Number of NN to use in model
+        :param knn_weights: How to weight the nearest neighbors. Check out sklearn for options
+        :param num_trees: Number of trees in the forest
+        :param max_depth: Max depth of any tree in the forest
         :return: None
         """
         if linreg:
@@ -33,27 +38,34 @@ class PredictQuery:
     def train_linreg(self, X, y):
         """
         Trains linear regression model
-        :param data: Training data
-        :return: handle to linear regression model
+        :param X: Training data features
+        :param y: Training data targets
+        :return: Handle to linear regression model
         """
         lr = LinearRegression()
         lr.fit(X, y)
         self.models["linreg"] = lr
+        return lr
 
     def train_knn(self, X, y, k=3, weights="uniform"):
         """
         Trains k nearest neighbor model.
+        :param X: Training data features
+        :param y: Training data targets
         :param k: Number of NN to use in model
-        :return: Handle to linear regression model
+        :param weights: How to weight the nearest neighbors. Check out sklearn for options
+        :return: Handle to knn model
         """
         knn = KNeighborsRegressor(n_neighbors=k, weights=weights)
         knn.fit(X, y)
         self.models["knn"] = knn
+        return knn
 
     def train_rf(self, X, y, num_trees=10, max_depth=None):
         """
         Trains random forest model
-        :param data: Training data
+        :param X: Training data features
+        :param y: Training data targets
         :param num_trees: Number of trees in the forest
         :param max_depth: Max depth of any tree in the forest
         :return: handle to random forest model
@@ -61,11 +73,13 @@ class PredictQuery:
         rf = RandomForestRegressor(n_estimators=num_trees, max_depth=max_depth)
         rf.fit(X, y)
         self.models["rf"] = rf
+        return rf
 
     def test(self, X, y):
         """
         Tests models
-        :param data: Testing data
+        :param X: Testing data features
+        :param y: Testing data targets
         :return: None
         """
         for key in self.models:
@@ -76,6 +90,10 @@ class PredictQuery:
 
 
 def test_on_diabetes():
+    """
+    Default function when file is ran. Tests regression models on sklearn diabetes dataset
+    :return: None
+    """
     # load dataset
     diabetes = datasets.load_diabetes()
 
