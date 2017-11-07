e52a070d80cf4ded370729faae549024554661a8
Aditya Sahai <aditya.sahai@gatech.edu>
2017-11-07 00:05:18 -0500

    fix database

---
+++ App.py
@@ -0,0 +1,9 @@
+import DBInteract
+
+def Main(ingredient_list):
+    '''
+    Returns the recipes for the given list of ingredients
+    :param ingredient_list: list of all ingredients
+    :return: list of ranked recipes
+    '''
+    
